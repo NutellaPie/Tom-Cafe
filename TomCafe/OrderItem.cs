a32bb1430f73fb3ac3da34c29b7d3aae8c5e3937
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-11 15:33:37 +0800

    Added OrderItem class + Properties

---
+++ TomCafe/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomCafe
+{
+    class OrderItem
+    {
+        //Properties
+        private int quantity;
+        private MenuItem item;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public MenuItem Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+
+    }
+}
