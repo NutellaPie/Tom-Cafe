187349b7394c1c00702f6747daa402f87632dfc8
Rui Jie Tay <s10178023@connect.np.edu.sg>
2018-01-11 15:16:25 +0800

    Created class Beverage

---
+++ TomCafe/Beverage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomCafe
+{
+    class Beverage:Product
+    {
+        //Create properties
+
+        private double tradeIn;
+
+        public double TradeIn
+        {
+            get { return tradeIn; }
+            set { tradeIn = value; }
+        }
+
+        //Create constructors
+
+        public Beverage() { }
+
+        public Beverage(string n, double p, double ti):base(n, p, ti)
+        {
+            TradeIn = ti;
+        }
+
+        public override double GetPrice()
+        {
+            return Price;
+        }
+
+        public override string ToString()
+        {
+            return TradeIn + base.ToString() + "\tTrade in amount $";
+        }
+
+    }
+}
