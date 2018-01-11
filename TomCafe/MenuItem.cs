f43ab2ec153b4ffc1cb9c6c760757128dfab09a2
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-11 15:25:29 +0800

    Created MenuItem + Added properties

---
+++ TomCafe/MenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomCafe
+{
+    class MenuItem
+    {
+        //Properties
+        private string name;
+        private double price;
+        private List<Product> productList;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public List<Product> ProductList
+        {
+            get { return productList; }
+            set { productList = value; }
+        }
+
+        
+    }
+}
