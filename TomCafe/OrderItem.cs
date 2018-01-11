a4cb04d863e9c50edcf81e296d33ef5dcad50a11
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-01-11 15:42:16 +0800

    Added constructors and methods to OrderItem

--- TomCafe/OrderItem.cs
+++ TomCafe/OrderItem.cs
@@ -23,6 +23,37 @@ namespace TomCafe
             set { item = value; }
         }
 
+        //Constructors
+        public OrderItem() { }
+        public OrderItem(MenuItem i)
+        {
+            Item = i;
+        }
 
+        //Methods
+        public void AddQty()
+        {
+            Quantity += 1;
+        }
+        public bool RemoveQty()
+        {
+            if (Quantity >= 1)
+            {
+                Quantity -= 1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double GetItemTotalAmt()
+        {
+            return Item.Price * Quantity;
+        }
+        public override string ToString()
+        {
+            return String.Format("Quantity: {0}\tItem: {1}", Quantity, Item);
+        }
     }
 }
