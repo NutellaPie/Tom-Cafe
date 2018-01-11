8caba2654e8b2c07a04c076fe957878a412c0847
Rui Jie Tay <s10178023@connect.np.edu.sg>
2018-01-11 15:50:30 +0800

    Created class Order

---
+++ TomCafe/Order.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomCafe
+{
+    class Order
+    {
+        //Create properties
+        private int orderNo;
+
+        public int OrderNo
+        {
+            get { return orderNo; }
+            set { orderNo = value; }
+        }
+
+        private List<OrderItem> itemList = new List<OrderItem>();
+
+        public List<OrderItem> ItemList
+        {
+            get { return itemList; }
+            set { itemList = value; }
+        }
+
+        //Create constructors
+        public Order() { }
+
+        public void Add(OrderItem oi)
+        {
+            ItemList.Add(oi);
+        }
+
+        public void Remove(int index)
+        {
+            itemList.RemoveAt(index);
+        }
+
+        public double GetTotalAmt()
+        {
+            double price = 0;
+
+            foreach (OrderItem o in ItemList)
+            {
+                price += o.GetItemTotalAmt();
+            }
+
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Order Number: " + OrderNo + "Item List: " + ItemList;
+        }
+
+    }
+}
