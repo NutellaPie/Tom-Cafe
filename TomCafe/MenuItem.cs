363b7e87a6698fc04703193081230c79de4136ca
Rui Jie Tay <s10178023@connect.np.edu.sg>
2018-01-17 14:23:34 +0800

    Removed ',' and ' ' for bundle meals

--- TomCafe/MenuItem.cs
+++ TomCafe/MenuItem.cs
@@ -50,6 +50,8 @@ namespace TomCafe
                 Items += p.Name + ", ";
             }
 
+            Items = Items.Trim(' ').Trim(',');
+
             return String.Format("{0}\n({1})\n${2:0.00}", Name, Items, Price);
         }
     }
